5066384ab1946b5b4ae74ca2218aabc659dc86f6
Gunanka D <gunanka2594@gmail.com>
2026-02-05 15:16:41 +0530

    fixed jbcrypt dependency. implemented signup and deleteByEmailId functions

--- backend/src/main/java/com/softvent/finflow/auth/AuthResource.java
+++ backend/src/main/java/com/softvent/finflow/auth/AuthResource.java
@@ -1,19 +1,67 @@
 package com.softvent.finflow.auth;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import org.mindrot.jbcrypt.BCrypt;
 
 import java.util.List;
 
 @Path("/auth")
+@Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class AuthResource {
 
     @GET
     public List<Auth> getAll() {
         return Auth.listAll();
+    }
+
+    @POST
+    @Path("/signup")
+    @Transactional
+    public Response signup(SignupRequest req) {
+
+        if (req == null || req.emailid == null || req.password == null) {
+            return Response.status(Response.Status.BAD_REQUEST).build(); // 400
+        }
+
+        boolean exists = Auth.find("emailid", req.emailid).firstResultOptional().isPresent();
+
+        // email id already present, make them login or try resetting password instead.
+        if (exists) {
+            return Response.status(Response.Status.CONFLICT).build(); // 409
+        }
+
+        // Auth class = Panache Entity representing Auth table but,
+        // Auth obj = A specific record that is being populated as shown below
+        Auth user = new Auth();
+        user.emailid = req.emailid;
+        user.pwdHash = BCrypt.hashpw(req.password, BCrypt.gensalt());
+
+        // Writes the record into the table
+        user.persist();
+
+        return Response.status(Response.Status.CREATED).build(); // 201
+    }
+
+    @DELETE
+    @Path("/delete/{emailid}")
+    @Transactional
+    public Response deleteByEmailId(@PathParam("emailid") String emailid){
+        if(emailid == null || emailid.isBlank()){
+            return Response.status(Response.Status.BAD_REQUEST).build(); // 400
+        }
+
+        long deleted = Auth.delete("emailid", emailid);
+
+        if(deleted == 0) {
+            return Response.status(Response.Status.NOT_FOUND).build(); // 404
+        }
+
+        return Response.status(Response.Status.NO_CONTENT).build(); // 204
     }
 }
 
---
+++ backend/src/main/java/com/softvent/finflow/auth/SignupRequest.java
@@ -0,0 +1,6 @@
+package com.softvent.finflow.auth;
+
+public class SignupRequest {
+    public String emailid;
+    public String password;
+}
