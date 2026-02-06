9fbe0bdaa4a8e9b5f601dd7ca55b75e28cb20efb
Gunanka D <gunanka2594@gmail.com>
2026-02-06 19:54:37 +0530

    Angular: Moved login and signup inside auth folder

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
-import { LoginComponent } from './features/login/login.component';
-import { SignupComponent } from './features/signup/signup.component'
+import { LoginComponent } from './features/auth/login/login.component';
+import { SignupComponent } from './features/auth/signup/signup.component'
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
