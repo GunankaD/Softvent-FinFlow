d4c1abe2ff238e783c3e72b907ee4502661c047c
Gunanka D <gunanka2594@gmail.com>
2026-02-10 23:14:58 +0530

    Angular: Implement footer component with responsive layout and styling

--- frontend/src/app/core/layout/footer/footer.component.ts
+++ frontend/src/app/core/layout/footer/footer.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  currentYear = new Date().getFullYear();
 }
