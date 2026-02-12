e4c4201473d6994542bbb081be5f719f0626b1be
Gunanka D <gunanka2594@gmail.com>
2026-02-12 16:21:02 +0530

    Angular: Fixed header to top and made sidebar close on choosing list items

--- frontend/src/app/core/layout/sidebar/sidebar.component.ts
+++ frontend/src/app/core/layout/sidebar/sidebar.component.ts
@@ -1,13 +1,18 @@
-import { Component, Input } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, RouterModule],
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent {
   @Input() isOpen = false;
+  @Output() linkClicked = new EventEmitter<void>();
+  
+  onLinkClick(): void {
+    this.linkClicked.emit();
+  }
 }
