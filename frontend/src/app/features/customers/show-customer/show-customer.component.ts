ee0bbdf8b6561bb7d4c38182b53e3277b0b32619
Gunanka D <gunanka2594@gmail.com>
2026-02-13 13:40:26 +0530

    Angular: Generated Show customer component

---
+++ frontend/src/app/features/customers/show-customer/show-customer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-show-customer',
+  imports: [],
+  templateUrl: './show-customer.component.html',
+  styleUrl: './show-customer.component.scss'
+})
+export class ShowCustomerComponent {
+
+}
