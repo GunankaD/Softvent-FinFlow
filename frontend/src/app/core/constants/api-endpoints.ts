5af7b3dd698940892f10d860661f054f9e26e57f
Gunanka D <gunanka2594@gmail.com>
2026-02-13 13:36:47 +0530

    Angular: Added Customer urls

--- frontend/src/app/core/constants/api-endpoints.ts
+++ frontend/src/app/core/constants/api-endpoints.ts
@@ -5,5 +5,13 @@ export const API_ENDPOINTS = {
     FORGOT_PASSWORD: '/auth/reset/forgot-password',
     RESET_PASSWORD: '/auth/reset/reset-password',
     EMAIL_FOR_TOKEN: '/auth/reset/reset-password/emailid'
+  },
+
+  CUSTOMERS: {
+    BASE: '/customers',
+    BY_ID: (id: number) => `/customers/id/${id}`, // dev only
+    BY_CCODE: (ccode: string) => `/customers/ccode/${ccode}`,
+    AVAILABILITY_CCODE: '/customers/availability/ccode',
+    AVAILABILITY_EMAIL: '/customers/availability/email'
   }
 } as const;
