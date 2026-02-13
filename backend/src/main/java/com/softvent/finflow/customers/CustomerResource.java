5d4fe11089485925491569fc787c5b6dfe87ac25
Gunanka D <gunanka2594@gmail.com>
2026-02-13 11:56:51 +0530

    Quarkus: Implemented CustomerResouce HTTP logic. Includes getters, creators and availability check functions

--- backend/src/main/java/com/softvent/finflow/customers/CustomerResource.java
+++ backend/src/main/java/com/softvent/finflow/customers/CustomerResource.java
@@ -1,18 +1,61 @@
 package com.softvent.finflow.customers;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import com.softvent.finflow.customers.dto.CustomerCreateRequest;
+import com.softvent.finflow.customers.dto.CustomerSummaryResponse;
+import com.softvent.finflow.customers.dto.CustomerDetailResponse;
+import jakarta.inject.Inject;
+import jakarta.validation.Valid;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 import java.util.List;
 
 @Path("/customers")
 @Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class CustomerResource {
 
-    @GET
-    public List<Customer> getAll() {
-        return Customer.listAll();
+    @Inject
+    CustomerService customerService;
+
+    // CREATE CUSTOMER ENTRY
+    @POST
+    public Response createCustomer(@Valid CustomerCreateRequest request) {
+
+        CustomerSummaryResponse response =
+                customerService.createCustomer(request);
+
+        return Response.status(Response.Status.CREATED)
+                .entity(response)
+                .build();
     }
+
+    // GET CUSTOMER INFO FUNCTIONS
+    @GET
+    @Path("/{id}")
+    public Response getCustomerById(@PathParam("id") Long id) {
+        CustomerDetailResponse response = customerService.getCustomerById(id);
+        return Response.ok(response).build();
+    }
+
+    @GET
+    public Response getAllCustomers() {
+        List<CustomerSummaryResponse> customers = customerService.getAllCustomers();
+        return Response.ok(customers).build();
+    }
+
+    // AVAILABILITY FUNCTIONS
+    @GET
+    @Path("availability/ccode")
+    public Response checkCcode(@QueryParam("ccode") String ccode) {
+        return Response.ok(customerService.checkCcodeAvailability(ccode)).build();
+    }
+
+    @GET
+    @Path("availability/email")
+    public Response checkEmail(@QueryParam("email") String email) {
+        return Response.ok(customerService.checkEmailAvailability(email)).build();
+    }
+
 }
