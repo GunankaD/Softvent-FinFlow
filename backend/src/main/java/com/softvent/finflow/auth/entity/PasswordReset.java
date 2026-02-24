f5f6d9fe507b5d6d3113542df1b0d11b1ad1ba8b
Gunanka D <gunanka2594@gmail.com>
2026-02-24 15:39:43 +0530

    Quarkus: Updated PasswordReset entity

--- backend/src/main/java/com/softvent/finflow/auth/entity/PasswordReset.java
+++ backend/src/main/java/com/softvent/finflow/auth/entity/PasswordReset.java
@@ -3,16 +3,17 @@ package com.softvent.finflow.auth.entity;
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import jakarta.persistence.*;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
 
 /*
-                                        Table "public.password_reset"
-   Column   |            Type             | Collation | Nullable |                  Default
-------------+-----------------------------+-----------+----------+--------------------------------------------
- id         | bigint                      |           | not null | nextval('password_reset_id_seq'::regclass)
- emailid    | character varying(50)       |           | not null |
- token      | character varying(255)      |           | not null |
- expires_at | timestamp without time zone |           | not null |
+                                       Table "public.password_reset"
+   Column   |           Type           | Collation | Nullable |                  Default
+------------+--------------------------+-----------+----------+--------------------------------------------
+ id         | bigint                   |           | not null | nextval('password_reset_id_seq'::regclass)
+ emailid    | character varying(50)    |           | not null |
+ token      | character varying(255)   |           | not null |
+ expires_at | timestamp with time zone |           | not null |
+ created_at | timestamp with time zone |           | not null | now()
 Indexes:
     "password_reset_pkey" PRIMARY KEY, btree (id)
     "password_reset_token_key" UNIQUE CONSTRAINT, btree (token)
@@ -32,6 +33,14 @@ public class PasswordReset extends PanacheEntityBase {
     @Column(nullable = false, length = 255, unique = true)
     public String token;
 
+    @Column(name = "created_at", nullable = false, updatable = false)
+    public Instant createdAt;
+
     @Column(name = "expires_at", nullable = false)
-    public LocalDateTime expiresAt;
+    public Instant expiresAt;
+
+    @PrePersist // RUNS BEFORE INSERT COMMANDS
+    public void prePersist() {
+        this.createdAt = Instant.now();
+    }
 }
