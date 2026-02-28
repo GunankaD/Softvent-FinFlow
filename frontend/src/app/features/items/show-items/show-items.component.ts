477a952d8d2ebe717736fa98c4e72c1085b64021
Gunanka D <gunanka2594@gmail.com>
2026-02-28 23:54:35 +0530

    Angular: Initialized show items component

---
+++ frontend/src/app/features/items/show-items/show-items.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-show-items',
+  imports: [],
+  templateUrl: './show-items.component.html',
+  styleUrl: './show-items.component.scss'
+})
+export class ShowItemsComponent {
+
+}
