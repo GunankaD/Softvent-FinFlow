aa9b4fa1c2a4273d872330de5475c989708c3544
Gunanka D <gunanka2594@gmail.com>
2026-03-05 18:28:20 +0530

    Angular: inMemoryScrolling enabled

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -16,7 +16,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(
       routes,
       withInMemoryScrolling({
-        scrollPositionRestoration: 'top'
+        scrollPositionRestoration: 'enabled'
       })
     ),
     provideHttpClient(),
