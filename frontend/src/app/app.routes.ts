da12ba35cbbf5be42f98621dd4a1dbdc44c22165
Gunanka D <gunanka2594@gmail.com>
2026-03-06 16:41:52 +0530

    Angular: Lazy loading all the components. Improves Initial load time and memory

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,59 +1,91 @@
 import { Routes } from '@angular/router';
-
 import { HomeComponent } from './features/home/home.component';
-
-// CUSTOMER ROUTES
-import { CustomersComponent } from './features/customers/customers.component';
-import { ShowCustomersComponent } from './features/customers/show-customers/show-customers.component';
-import { AddCustomerComponent } from './features/customers/add-customer/add-customer.component';
-
-// AUTH ROUTES
-import { LoginComponent } from './auth/login/login.component';
-import { SignupComponent } from './auth/signup/signup.component'
-import { SignupInitComponent } from './auth/signup-init/signup-init.component';
-import { ForgotPasswordComponent } from './auth/forgot-password/forgot-password.component';
-import { ResetPasswordComponent } from './auth/reset-password/reset-password.component';
 
 export const routes: Routes = [
     // HOME
-    {path: '', component: HomeComponent},
+    { path: '', component: HomeComponent },
 
-    // AUTH
-    {path: 'login', component: LoginComponent},
-    {path: 'signup', component: SignupComponent },
-    {path: 'signup-init', component: SignupInitComponent},
-    {path: 'forgot-password', component: ForgotPasswordComponent},
-    {path: 'reset-password', component: ResetPasswordComponent},
+    // AUTH ROUTES
+    {
+        path: 'login',
+        loadComponent: () =>
+            import('./auth/login/login.component')
+            .then(m => m.LoginComponent)
+    },
+    {
+        path: 'signup',
+        loadComponent: () =>
+            import('./auth/signup/signup.component')
+            .then(m => m.SignupComponent)
+    },
+    {
+        path: 'signup-init',
+        loadComponent: () =>
+            import('./auth/signup-init/signup-init.component')
+            .then(m => m.SignupInitComponent)
+    },
+    {
+        path: 'forgot-password',
+        loadComponent: () =>
+            import('./auth/forgot-password/forgot-password.component')
+            .then(m => m.ForgotPasswordComponent)
+    },
+    {
+        path: 'reset-password',
+        loadComponent: () =>
+            import('./auth/reset-password/reset-password.component')
+            .then(m => m.ResetPasswordComponent)
+    },
 
-    // CUSTOMERS
-    {path: 'customers', component: CustomersComponent},
-    {path: 'customers/show-customers', component: ShowCustomersComponent},
-    {path: 'customers/add-customer', component: AddCustomerComponent},
-    {path: 'customers/:ccode',
+    // CUSTOMER ROUTES
+    {
+        path: 'customers',
+        loadComponent: () =>
+            import('./features/customers/customers.component')
+            .then(m => m.CustomersComponent)
+    },
+    {
+        path: 'customers/show-customers',
+        loadComponent: () =>
+            import('./features/customers/show-customers/show-customers.component')
+            .then(m => m.ShowCustomersComponent)
+    },
+    {
+        path: 'customers/add-customer',
+        loadComponent: () =>
+            import('./features/customers/add-customer/add-customer.component')
+            .then(m => m.AddCustomerComponent)
+    },
+    {
+        path: 'customers/:ccode',
         loadComponent: () =>
             import('./features/customers/customer-details/customer-details.component')
             .then(m => m.CustomerDetailsComponent)
     },
-    {path: 'items',
+
+    // ITEM ROUTES
+    {
+        path: 'items',
         loadComponent: () =>
             import('./features/items/items.component')
             .then(m => m.ItemsComponent)
     },
     {
-    path: 'items/add-item',
-    loadComponent: () =>
-        import('./features/items/add-item/add-item.component')
-        .then(m => m.AddItemComponent)
+        path: 'items/add-item',
+        loadComponent: () =>
+            import('./features/items/add-item/add-item.component')
+            .then(m => m.AddItemComponent)
     },
-    {path: 'items/show-items',
+    {
+        path: 'items/show-items',
         loadComponent: () =>
             import('./features/items/show-items/show-items.component')
             .then(m => m.ShowItemsComponent)
     },
-    {path: 'items/:icode',
+    {
+        path: 'items/:icode',
         loadComponent: () =>
             import('./features/items/item-details/item-details.component')
             .then(m => m.ItemDetailsComponent)
     },
 ];
-
