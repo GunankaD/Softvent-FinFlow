6ae32ff3b8466d860c4e3816ed249e92fca603c6
Gunanka D <gunanka2594@gmail.com>
2026-03-07 16:30:04 +0530

    Angular: Added new endpoints

--- frontend/src/app/core/constants/api-endpoints.ts
+++ frontend/src/app/core/constants/api-endpoints.ts
@@ -1,6 +1,8 @@
 export const API_ENDPOINTS = {
   AUTH: {
     LOGIN: '/auth/login',
+    REFRESH: '/auth/refresh',
+    LOGOUT: '/auth/logout',
     FORGOT_PASSWORD: '/auth/reset/forgot-password',
     RESET_PASSWORD: '/auth/reset/reset-password',
     EMAIL_FOR_TOKEN: '/auth/reset/reset-password/email',
