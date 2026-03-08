d69b73c577160419320a8745e7c986fb4eca9720
Gunanka D <gunanka2594@gmail.com>
2026-03-08 19:28:43 +0530

    Angular: Implemented logout button

--- frontend/src/app/core/layout/header/header.component.ts
+++ frontend/src/app/core/layout/header/header.component.ts
@@ -1,11 +1,18 @@
 // ANGULAR
 import { Component, EventEmitter, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink, RouterModule } from '@angular/router';
 
 // MATERIAL UI
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../../services/auth/auth.service';
+import { SnackbarService } from '../../services/snackbar/snackbar.service';
 
 @Component({
   selector: 'app-header',
@@ -17,13 +24,46 @@ import {MatToolbarModule} from '@angular/material/toolbar';
     RouterModule,
     MatIconModule,
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    CommonModule
   ]
 })
 export class HeaderComponent {
   @Output() toggleSidebar = new EventEmitter<void>();
 
+  private readonly authService = inject(AuthService);
+  private readonly router = inject(Router);
+  private readonly snackbar = inject(SnackbarService);
+
   onToggleSidebar(): void {
     this.toggleSidebar.emit();
+  }
+
+  logout(): void {
+
+    this.authService.logout().subscribe({
+      next: () => {
+        this.authService.clearSession();
+        this.snackbar.success('Logged out successfully', 4000);
+        this.router.navigate(['/login']);
+      },
+      error: () => {
+        // even if backend fails, clear session
+        this.authService.clearSession();
+        this.router.navigate(['/login']);
+      }
+    });
+
+  }
+
+  isAuthPage(): boolean {
+    const url = this.router.url;
+
+    return (
+      url.includes('/login') ||
+      url.includes('/signup') ||
+      url.includes('/forgot-password') ||
+      url.includes('/reset-password')
+    );
   }
 }
