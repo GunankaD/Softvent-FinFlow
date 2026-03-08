b9971a988033e48d051abc2939f27563677313b9
Gunanka D <gunanka2594@gmail.com>
2026-03-08 20:03:37 +0530

    Angular: Implemented BreadCrumb Component

--- frontend/src/app/shared/components/breadcrumb/breadcrumb.component.ts
+++ frontend/src/app/shared/components/breadcrumb/breadcrumb.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { Breadcrumb } from '../models/breadcrumb.model';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-breadcrumb',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule, CommonModule, MatIconModule],
   templateUrl: './breadcrumb.component.html',
   styleUrl: './breadcrumb.component.scss'
 })
 export class BreadcrumbComponent {
+
+  @Input({ required: true })
+  public items: Breadcrumb[] = [];
 
 }
