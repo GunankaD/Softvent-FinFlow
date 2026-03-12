4818ad847bcd7789367d98a29491f1a94d29dfa8
Gunanka D <gunanka2594@gmail.com>
2026-03-12 20:12:02 +0530

    Quarkus: Created Enums

---
+++ backend/src/main/java/com/softvent/finflow/transactions/enums/InvoiceStatus.java
@@ -0,0 +1,10 @@
+package com.softvent.finflow.transactions.enums;
+
+public enum InvoiceStatus {
+
+    DRAFT,
+    UNPAID,
+    PARTIAL,
+    PAID,
+    VOID
+}
---
+++ backend/src/main/java/com/softvent/finflow/transactions/enums/PaymentMode.java
@@ -0,0 +1,10 @@
+package com.softvent.finflow.transactions.enums;
+
+public enum PaymentMode {
+
+    CASH,
+    UPI,
+    CARD,
+    BANK_TRANSFER,
+    CHEQUE
+}
