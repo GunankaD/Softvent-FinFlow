5296addddb727fb98e77fe3610da201c80d7e0bb
Gunanka D <gunanka2594@gmail.com>
2026-03-18 16:38:29 +0530

    Quarkus: Add ReceiptCreateResponse DTO for handling receipt creation responses

---
+++ backend/src/main/java/com/softvent/finflow/transactions/receipts/dto/ReceiptCreateResponse.java
@@ -0,0 +1,9 @@
+package com.softvent.finflow.transactions.receipts.dto;
+
+public class ReceiptCreateResponse {
+
+    public String receiptNumber;
+    public ReceiptCreateResponse(String receiptNumber) {
+        this.receiptNumber = receiptNumber;
+    }
+}
