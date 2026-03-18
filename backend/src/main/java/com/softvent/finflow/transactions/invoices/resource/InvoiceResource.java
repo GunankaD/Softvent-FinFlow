9938da99c4025c76699991d3338c442548c88bb5
Gunanka D <gunanka2594@gmail.com>
2026-03-18 16:57:14 +0530

    Quarkus: Refactor InvoiceResource: Add validation to createInvoice method and streamline response handling

--- backend/src/main/java/com/softvent/finflow/transactions/invoices/resource/InvoiceResource.java
+++ backend/src/main/java/com/softvent/finflow/transactions/invoices/resource/InvoiceResource.java
@@ -8,6 +8,7 @@ import com.softvent.finflow.transactions.invoices.service.InvoiceService;
 
 import jakarta.annotation.security.RolesAllowed;
 import jakarta.inject.Inject;
+import jakarta.validation.Valid;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
@@ -25,10 +26,8 @@ public class InvoiceResource {
 
     // CREATE
     @POST
-    public Response create(InvoiceCreateRequest request) {
-
+    public Response create(@Valid InvoiceCreateRequest request) {
         InvoiceCreateResponse response = invoiceService.createInvoice(request);
-
         return Response
                 .status(Response.Status.CREATED)
                 .entity(response)
@@ -36,35 +35,25 @@ public class InvoiceResource {
     }
 
     // GETTERS
-
     @GET
     public Response getAll() {
-
         List<InvoiceSummaryResponse> response = invoiceService.getInvoices();
-
         return Response.ok(response).build(); // 200
     }
 
     @GET
     @Path("/{invoiceNumber}")
-    public Response getByInvoiceNumber(
-            @PathParam("invoiceNumber") String invoiceNumber) {
-
+    public Response getByInvoiceNumber(@PathParam("invoiceNumber") String invoiceNumber) {
         InvoiceDetailResponse response =
                 invoiceService.getInvoiceByNumber(invoiceNumber);
-
         return Response.ok(response).build(); // 200
     }
 
     // SOFT DELETE
-
     @DELETE
     @Path("/{invoiceNumber}")
-    public Response voidInvoice(
-            @PathParam("invoiceNumber") String invoiceNumber) {
-
+    public Response voidInvoice(@PathParam("invoiceNumber") String invoiceNumber) {
         invoiceService.deleteInvoiceByNumber(invoiceNumber);
-
         return Response.noContent().build(); // 204
     }
 }
