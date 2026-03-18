60918e1246fb314b87a0e00e78e2e3fe4b347c3b
Gunanka D <gunanka2594@gmail.com>
2026-03-18 16:57:21 +0530

    Quarkus: Implement ReceiptResource with CRUD operations for receipts including validation and response handling

---
+++ backend/src/main/java/com/softvent/finflow/transactions/receipts/resource/ReceiptResource.java
@@ -0,0 +1,59 @@
+package com.softvent.finflow.transactions.receipts.resource;
+
+import com.softvent.finflow.transactions.receipts.dto.ReceiptCreateRequest;
+import com.softvent.finflow.transactions.receipts.dto.ReceiptCreateResponse;
+import com.softvent.finflow.transactions.receipts.dto.ReceiptDetailResponse;
+import com.softvent.finflow.transactions.receipts.dto.ReceiptSummaryResponse;
+import com.softvent.finflow.transactions.receipts.service.ReceiptService;
+
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.inject.Inject;
+import jakarta.validation.Valid;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import java.util.List;
+
+@RolesAllowed("USER")
+@Path("/receipts")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class ReceiptResource {
+
+    @Inject
+    ReceiptService receiptService;
+
+    // CREATE
+    @POST
+    public Response create(@Valid ReceiptCreateRequest request) {
+        ReceiptCreateResponse response = receiptService.createReceipt(request);
+        return Response
+                .status(Response.Status.CREATED)
+                .entity(response)
+                .build(); // 201
+    }
+
+    // GETTERS
+    @GET
+    public Response getAll() {
+        List<ReceiptSummaryResponse> response = receiptService.getReceipts();
+        return Response.ok(response).build(); // 200
+    }
+
+    @GET
+    @Path("/{receiptNumber}")
+    public Response getByReceiptNumber(@PathParam("receiptNumber") String receiptNumber) {
+        ReceiptDetailResponse response =
+                receiptService.getReceiptByNumber(receiptNumber);
+        return Response.ok(response).build(); // 200
+    }
+
+    // SOFT DELETE
+    @DELETE
+    @Path("/{receiptNumber}")
+    public Response delete(@PathParam("receiptNumber") String receiptNumber) {
+        receiptService.deleteReceiptByNumber(receiptNumber);
+        return Response.noContent().build(); // 204
+    }
+}
