44938f684aea9956a31dce3259782edee7750345
Gunanka D <gunanka2594@gmail.com>
2026-03-19 16:46:45 +0530

    Quarkus: Change invoiceNumber field to public in InvoiceCreateResponse for jackson serializer to access this field

--- backend/src/main/java/com/softvent/finflow/transactions/invoices/dto/InvoiceCreateResponse.java
+++ backend/src/main/java/com/softvent/finflow/transactions/invoices/dto/InvoiceCreateResponse.java
@@ -1,7 +1,7 @@
 package com.softvent.finflow.transactions.invoices.dto;
 
 public class InvoiceCreateResponse {
-    String invoiceNumber;
+    public String invoiceNumber;
     public InvoiceCreateResponse(String invoiceNumber){
         this.invoiceNumber = invoiceNumber;
     }
