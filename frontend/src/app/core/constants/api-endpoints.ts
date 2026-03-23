806b47d8d25b63df23a8a1d8d6e654a7290ff85c
Gunanka D <gunanka2594@gmail.com>
2026-03-23 19:47:45 +0530

    Angular: Added transaction endpoints

--- frontend/src/app/core/constants/api-endpoints.ts
+++ frontend/src/app/core/constants/api-endpoints.ts
@@ -16,7 +16,9 @@ export const API_ENDPOINTS = {
     BY_ID: (id: number) => `/customers/id/${id}`, // dev only
     BY_CCODE: (ccode: string) => `/customers/${ccode}`,
     AVAILABILITY_CCODE: '/customers/availability/ccode',
-    AVAILABILITY_EMAIL: '/customers/availability/email'
+    AVAILABILITY_EMAIL: '/customers/availability/email',
+    INVOICES: (ccode: string, filter?: string) => `/customers/${ccode}/invoices${filter ? `?filter=${filter}` : ''}`,
+    RECEIPTS: (ccode: string, filter?: string) => `/customers/${ccode}/receipts${filter ? `?filter=${filter}` : ''}`,
   },
   
   ITEMS: {
@@ -28,5 +30,20 @@ export const API_ENDPOINTS = {
   ITEM_GROUPS: {
     BASE: '/item-groups',
     BY_IGD: (igid: number) => `/item-groups/${igid}`
+  },
+
+  INVOICES: {
+    BASE: '/invoices',
+    BY_NUMBER: (invoiceNumber: string) => `/invoices/${invoiceNumber}`
+  },
+
+  RECEIPTS: {
+    BASE: '/receipts',
+    BY_NUMBER: (receiptNumber: string) => `/receipts/${receiptNumber}`
+  },
+
+  PAYMENT_APPLICATIONS: {
+    APPLY: '/payments/apply'
   }
+
 } as const;
