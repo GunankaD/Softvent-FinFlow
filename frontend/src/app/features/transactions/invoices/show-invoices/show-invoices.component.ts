4f015d3565317d664c41df511ed6880646d2740e
Gunanka D <gunanka2594@gmail.com>
2026-03-26 16:42:08 +0530

    Angular: Generated show invoices and add invoice components

---
+++ frontend/src/app/features/transactions/invoices/show-invoices/show-invoices.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-show-invoices',
+  imports: [],
+  templateUrl: './show-invoices.component.html',
+  styleUrl: './show-invoices.component.scss'
+})
+export class ShowInvoicesComponent {
+
+}
