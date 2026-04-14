bbc8447cd534b0d8c6677cd9e3c80383fbf6b54c
Gunanka D <gunanka2594@gmail.com>
2026-04-14 11:29:31 +0530

    Angular: Initialized payment application component

---
+++ frontend/src/app/features/transactions/payment-application/payment-application.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-application',
+  imports: [],
+  templateUrl: './payment-application.component.html',
+  styleUrl: './payment-application.component.scss'
+})
+export class PaymentApplicationComponent {
+
+}
